c6585a397118db47094455b9773f401b9f49e942
7om+ <101679156+7omx@users.noreply.github.com>
2023-05-16 09:49:41 -0300

    crud

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const server = express()
+
+server.use(express.json());
+
+const filmes = ['harry potter', 'Jogos vorazes', 'Avengers', 'chuck', 'Godzilla', 'Senhor dos anéis']
+
+
+//retorna apenas um filme
+server.get('/filmes/:index', (req, res) => {
+    const { index } = req.params;
+    return res.json(filmes[index]);
+});
+
+
+//apresenta  todos
+server.get('/filmes', (req, res) => {
+    return res.json(filmes);
+});
+
+//coloca um novo filme
+server.post('/filmes', (req, res) => {
+    const { name } = req.body;
+    filmes.push(name);
+
+    return res.json(filmes);
+});
+
+//atualizar
+server.put('/filmes/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    filmes[index] = name;
+    return res.json(filmes);
+});
+
+server.delete('/filmes/:index', (req, res) => {
+    const { index } = req.params;
+
+    filmes.splice(index, 1);
+    return res.json({ message: "o curso foi apagado" });
+});
+
+server.listen(3000);
